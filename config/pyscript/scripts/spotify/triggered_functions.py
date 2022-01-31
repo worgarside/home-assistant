4c69bdd00481fb0e9ccab3fae6bed9ed75ffae90
Will Garside <worgarside@gmail.com>
2022-01-31 17:16:40 +0000

    Tidy up Spotify monthly function(s)

--- config/pyscript/scripts/spotify/triggered_functions.py
+++ config/pyscript/scripts/spotify/triggered_functions.py
@@ -1,10 +1,16 @@
 """Functions which are only run on a certain trigger"""
-from socket import gethostname
+from datetime import datetime
 from os.path import isfile
-
+from socket import gethostname
+from re import compile as compile_regex
 from requests import get
+from wg_utilities.clients.spotify_client import SpotifyClient
 from wg_utilities.functions import force_mkdir
+
+from helpers import get_secret
 from helpers import write_file
+
+MODULE_NAME = "spotify"
 
 if gethostname() != "homeassistant":
     from helpers import local_setup
@@ -13,7 +19,106 @@ if gethostname() != "homeassistant":
     task = async_mock
     state = sync_mock
     sensor = sync_mock
+    persistent_notification = sync_mock
     state_trigger = decorator
+    pyscript_executor = decorator
+    time_trigger = decorator
+
+SPOTIFY = SpotifyClient(
+    client_id=get_secret("client_id", module=MODULE_NAME),
+    client_secret=get_secret("client_secret", module=MODULE_NAME),
+    scope=SpotifyClient.ALL_SCOPES,
+    creds_cache_path="/config/.spotify_cache",
+)
+
+_MONTH_LIST = "|".join(
+    datetime.strptime(str(i), "%m").strftime("%B") for i in range(1, 13)
+)
+MONTHLY_PATTERN = compile_regex(rf"^({_MONTH_LIST}) '[0-9]{{2}}$")
+
+
+@pyscript_executor
+def get_monthly_playlists(return_count=12):
+    """Gets all monthly playlists from Spotify (based on name)
+
+    Args:
+        return_count (int): the number of playlists to return (ordered by datetime,
+         descending)
+
+    Returns:
+        list: a list of Playlist instances
+    """
+
+    return sorted(
+        [p for p in SPOTIFY.playlists if MONTHLY_PATTERN.match(p.name)],
+        key=lambda playlist: datetime.strptime(playlist.name, "%B '%y"),
+    )[-return_count:]
+
+
+@time_trigger("cron(*/15 * * * *)")
+def update_spotify_monthlies():
+    """Updates all Spotify monthly playlists!"""
+
+    log.info("Resetting all Spotify client properties")
+    SPOTIFY.reset_properties()
+
+    recently_liked = task.executor(SPOTIFY.get_recently_liked_tracks, day_limit=7)
+
+    all_monthly_playlist_track_ids = set()
+    for playlist in get_monthly_playlists(6):
+        all_monthly_playlist_track_ids.update(
+            [t.id for t in task.executor(getattr, playlist, "tracks")]
+        )
+
+    playlist_updates = {}
+
+    for track in recently_liked:
+        if track.id not in all_monthly_playlist_track_ids:
+            log.debug("Processing %s", str(track))
+
+            target_playlist_name = datetime.strptime(
+                track.metadata.get("liked_at"), SPOTIFY.DATETIME_FORMAT
+            ).strftime("%B '%y")
+
+            for playlist in playlist_updates:
+                if playlist.name == target_playlist_name:
+                    target_monthly_playlist = playlist
+                    break
+            else:
+                target_monthly_playlist = task.executor(
+                    SPOTIFY.get_playlists_by_name, target_playlist_name
+                )
+
+                if target_monthly_playlist is None:
+                    target_monthly_playlist = task.executor(
+                        SPOTIFY.create_playlist,
+                        name=target_playlist_name,
+                        public=False,
+                        collaborative=False,
+                    )
+
+            if target_monthly_playlist not in playlist_updates:
+                playlist_updates[target_monthly_playlist] = []
+
+            playlist_updates[target_monthly_playlist].append(track)
+
+    for playlist, tracks in playlist_updates.items():
+        log.debug("Adding %s to %s", ", ".join(map(str, tracks)), playlist.name)
+        task.executor(SPOTIFY.add_tracks_to_playlist, tracks, playlist)
+
+    if playlist_updates:
+        message = ""
+
+        for playlist, tracks in playlist_updates.items():
+            message += f"### {playlist.name}\n"
+            for track in tracks:
+                message += f"""- {track.name}\n"""
+
+        persistent_notification.create(
+            title="Spotify Monthlies Updated", message=message
+        )
+
+    log.info("Process complete, %i API calls made in total", SPOTIFY.api_call_count)
 
 
 @state_trigger("sensor.spotify_matt_scott_media_album_artwork_internal_url")
