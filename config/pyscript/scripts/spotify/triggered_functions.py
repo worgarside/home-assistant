b19d476900adf3ff6639c62bf06ba1c5256d0624
Will Garside <worgarside@gmail.com>
2022-04-12 22:37:32 +0100

    Add function for adding recently liked songs to CE/JJ playlist(s)

--- config/pyscript/scripts/spotify/triggered_functions.py
+++ config/pyscript/scripts/spotify/triggered_functions.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from os.path import isfile
 from socket import gethostname
 from re import compile as compile_regex
+
 from requests import get
 from wg_utilities.clients import SpotifyClient
 from wg_utilities.functions import force_mkdir
@@ -10,19 +11,21 @@ from wg_utilities.functions import force_mkdir
 from helpers import get_secret
 from helpers import write_file
 
+
 MODULE_NAME = "spotify"
 
 if gethostname() != "homeassistant":
     from helpers import local_setup
 
-    log, async_mock, sync_mock, decorator = local_setup()
-    task = async_mock
+    log, task, sync_mock, decorator = local_setup()
     state = sync_mock
     sensor = sync_mock
     persistent_notification = sync_mock
+    notify = sync_mock
     state_trigger = decorator
     pyscript_executor = decorator
     time_trigger = decorator
+    event_trigger = decorator
 
 SPOTIFY = SpotifyClient(
     client_id=get_secret("client_id", module=MODULE_NAME),
@@ -35,6 +38,10 @@ _MONTH_LIST = "|".join(
     [datetime.strptime(str(i), "%m").strftime("%B") for i in range(1, 13)]
 )
 MONTHLY_PATTERN = compile_regex(rf"^({_MONTH_LIST}) '[0-9]{{2}}$")
+
+# Playlists
+CHILL_ELECTRONICA = task.executor(SPOTIFY.get_playlist_by_id, "2lMx8FU0SeQ7eA5kcMlNpX")
+JAMBOX_JAMS = task.executor(SPOTIFY.get_playlist_by_id, "4Vv023MaZsc8NTWZ4WJvIL")
 
 
 @pyscript_executor
@@ -112,7 +119,24 @@ def update_spotify_monthlies():
         for playlist, tracks in playlist_updates.items():
             message += f"### {playlist.name}\n"
             for track in tracks:
-                message += f"""- {track.name}\n"""
+                message += f"- {track.name}\n"
+
+                notify.mobile_app_will_s_pixel_6_pro(
+                    message=f'"{track.name}" added to {playlist.name}',
+                    data={
+                        "actions": [
+                            {
+                                "action": "ADD_SONG_TO_CHILL_ELECTRONICA",
+                                "title": "Chill Electronica",
+                            },
+                            {
+                                "action": "ADD_SONG_TO_JAMBOX_JAMS",
+                                "title": "JAMBOX Jams",
+                            },
+                            {"action": "ADD_SONG_TO_BOTH", "title": "Both"},
+                        ]
+                    },
+                )
 
         persistent_notification.create(
             title="Spotify Monthlies Updated", message=message
@@ -181,3 +205,51 @@ def save_album_artwork(var_name, value, old_value):
         res.content,
         encoding=None,
     )
+
+
+@event_trigger("mobile_app_notification_action")
+def add_track_to_playlist(action, message, **_):
+    """Add a given track to a playlist
+
+    Args:
+        action (str): the action from the mobile notification
+        message (str): the message displayed within the notification
+        **_: any other kwargs
+
+    Raises:
+        Exception: if the desired track can't be found in the recently liked
+    """
+
+    # Other mobile notifications actions could trigger this function
+    if not action.startswith("ADD_SONG_TO_"):
+        return
+
+    target_track_name = message.split(" added to ")[0].strip('"')
+
+    log.debug("Looking for '%s'", target_track_name)
+
+    track = None
+    for track in task.executor(SPOTIFY.get_recently_liked_tracks, day_limit=7):
+        if track.name == target_track_name:
+            break
+
+    if not track:
+        raise Exception(
+            f'Unable to find track "{target_track_name}" in recently liked!'
+        )
+
+    if action == "ADD_SONG_TO_BOTH":
+        task.executor(
+            SPOTIFY.add_tracks_to_playlist, tracks=[track], playlist=CHILL_ELECTRONICA
+        )
+        task.executor(
+            SPOTIFY.add_tracks_to_playlist, tracks=[track], playlist=JAMBOX_JAMS
+        )
+    elif action == "ADD_SONG_TO_CHILL_ELECTRONICA":
+        task.executor(
+            SPOTIFY.add_tracks_to_playlist, tracks=[track], playlist=CHILL_ELECTRONICA
+        )
+    elif action == "ADD_SONG_TO_JAMBOX_JAMS":
+        task.executor(
+            SPOTIFY.add_tracks_to_playlist, tracks=[track], playlist=JAMBOX_JAMS
+        )
