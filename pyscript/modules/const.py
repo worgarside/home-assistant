d6da7056cac7823cf7ec88197fc2a9afe6acb5eb
Will Garside <worgarside@gmail.com>
2023-10-04 19:27:22 +0100

    Remove Pyscript entirely

--- pyscript/modules/const.py
+++
@@ -1,8 +0,0 @@
-"""Constants for the Pyscript integration."""
-from __future__ import annotations
-
-from os import environ
-
-OAUTH_CREDS_CACHE_DIR = "/config/.wg-utilities/oauth_credentials"
-
-environ["WG_UTILITIES_CREDS_CACHE_DIR"] = str(OAUTH_CREDS_CACHE_DIR)
