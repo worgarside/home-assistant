2a23ccf9e23956442299f8f4bd925feee61df5f3
Will Botside <worgarside.dev@gmail.com>
2023-10-28 18:04:44 +0100

    Add webhook to get latest release/git command updates (#500)
    * Add webhook to get latest release
    
    * Fix bad file path
    
    * Add payload validation and notification
    
    * Add sensors for current git ref/branch
    
    ---------
    
    Co-authored-by: Will Garside <worgarside@gmail.com>

--- resources/shell_command/git/get_latest_release.sh
+++ resources/shell_command/git/get_latest_release.sh
@@ -4,12 +4,16 @@
     exec > >(trap "" INT TERM; sed "s/^/$(date +'%Y-%m-%dT%H:%M:%S')\t[INFO]\t/")
     exec 2> >(trap "" INT TERM; sed "s/^/$(date +'%Y-%m-%dT%H:%M:%S')\t[ERROR]\t/" >&2)
 
+    cd /config || exit 1
+
     echo git add .
     git add .
 
-    echo git stash -m "shell_command.checkout_develop_head|$(date +%s)"
-    git stash -m "shell_command.checkout_develop_head|$(date +%s)"
+    echo git stash -m "shell_command.get_latest_release|$(date +%s)"
+    git stash -m "shell_command.get_latest_release|$(date +%s)"
 
-    git checkout develop
+    git checkout main
     git pull
+
+    git status
 }>> /config/shell-command.log 2>&1
