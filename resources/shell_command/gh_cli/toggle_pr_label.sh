8d52d42f9aab7c8ee1300bfdcb7c55f07acabb0f
Will Botside <worgarside.dev@gmail.com>
2023-12-07 14:25:34 +0000

    Upgrade CICD/Add  local Git control entities (#630)
    * Add entities for Git branches
    
    * Fix alias snake_case -> kebab-case
    
    * Add checkout functionality via `input_select.target_git_branch`
    
    * Fix incorrect notification title
    
    * Fix bad shell command file path
    
    * Streamline CI/CD Git commands
    
    * Add `script.git_pull`
    
    * Add `beautysh` PCH
    
    * Ensure input select options are set on restart
    
    * Only auto-checkout new branches
    
    * Add defaults to git checkout shell command
    
    * Update logging/default values
    
    ---------
    
    Co-authored-by: Will Garside <worgarside@gmail.com>

--- resources/shell_command/gh_cli/toggle_pr_label.sh
+++ resources/shell_command/gh_cli/toggle_pr_label.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 while getopts ":u:l:a:" opt; do
-  case $opt in
-    u) URL="$OPTARG"
-    ;;
-    l) LABEL="$OPTARG"
-    ;;
-    a) ADD_REMOVE="$OPTARG"
-    ;;
-    *) echo "shell_command.toggle_pr_label INVALID USAGE" >> /config/home-assistant.log
-       exit 1 ;;
-  esac
+    case $opt in
+        u) URL="$OPTARG"
+            ;;
+        l) LABEL="$OPTARG"
+            ;;
+        a) ADD_REMOVE="$OPTARG"
+            ;;
+        *) echo "shell_command.toggle_pr_label INVALID USAGE" >> /config/home-assistant.log
+            exit 1 ;;
+    esac
 done
 
 /config/resources/gh_cli/bin/gh pr edit "$URL" --"$ADD_REMOVE"-label "$LABEL"
---
+++ resources/shell_command/git/checkout_git_branch.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+BRANCH="main"
+TOKEN=""
+ENTITY_ID=""
+
+while getopts ":b:e:t:" opt; do
+    case ${opt} in
+        b)
+            if [[ -n "${OPTARG}" ]]; then
+                BRANCH="${OPTARG}"
+            fi
+            ;;
+        e) ENTITY_ID="${OPTARG}" ;;
+        t) TOKEN="${OPTARG}" ;;
+        *) echo "shell_command.checkout_git_branch INVALID USAGE: -${opt} ${OPTARG}" >> /config/home-assistant.log
+            exit 1 ;;
+    esac
+done
+
+{
+    exec > >(trap "" INT TERM; sed "s/^/$(date +'%Y-%m-%dT%H:%M:%S')\t[INFO]\t/")
+    exec 2> >(trap "" INT TERM; sed "s/^/$(date +'%Y-%m-%dT%H:%M:%S')\t[ERROR]\t/" >&2)
+
+    echo "shell_command.checkout_git_branch STARTED with branch: ${BRANCH} and entity ID(s): ${ENTITY_ID}"
+
+    cd /config || exit 1
+
+    echo git add .
+    git add .
+
+    echo git stash -m "shell_command.checkout_git_branch|${BRANCH}|$(date +%s)"
+    git stash -m "shell_command.checkout_git_branch|${BRANCH}|$(date +%s)"
+
+    echo git fetch --all --prune
+    git fetch --all --prune
+
+    echo git checkout "${BRANCH}"
+    git checkout "${BRANCH}"
+
+    echo git pull
+    git pull
+
+    git status
+
+    if [[ -n ${TOKEN} ]] && [[ -n ${ENTITY_ID} ]]; then
+        IFS=',' read -ra ENTITY_IDS <<< "${ENTITY_ID}"
+
+        for id in "${ENTITY_IDS[@]}"; do
+            echo "Updating Home Assistant entity: ${id}"
+
+            curl -sSL -X POST \
+                -H "Authorization: Bearer ${TOKEN}" \
+                -H "Content-Type: application/json" \
+                -d '{"entity_id": "'"${id}"'"}' \
+                http://homeassistant.local:8123/api/services/homeassistant/update_entity
+        done
+
+    else
+        echo "Token or entity ID missing, skipping Home Assistant update."
+    fi
+
+
+}>> /config/shell-command.log 2>&1
